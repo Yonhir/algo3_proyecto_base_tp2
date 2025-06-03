243a98ffe78dc6126061a40b7c817348e80e3e45
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-02 22:35:36 -0300

    primeras implementaciones mazos

---
+++ src/main/java/edu/fiuba/algo3/CardCollection.java
@@ -0,0 +1,15 @@
+package edu.fiuba.algo3;
+
+import java.util.List;
+
+public abstract class CardCollection {
+    private List<Card> cards;
+
+    public CardCollection(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public int getCardCount() {
+        return cards.size();
+    }
+}
---
+++ src/main/java/edu/fiuba/algo3/Deck.java
@@ -0,0 +1,9 @@
+package edu.fiuba.algo3;
+
+import java.util.List;
+
+public class Deck extends CardCollection {
+    public Deck(List<Card> cards) {
+        super(cards);
+    }
+}
