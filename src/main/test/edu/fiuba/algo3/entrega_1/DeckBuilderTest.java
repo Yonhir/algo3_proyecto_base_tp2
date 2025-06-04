7aebc47d11d181a06d5e4dbdad77ccce4973d2ca
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-04 17:16:06 -0300

    si se quiere crear el mazo con insuficientes cartas especiales o unidades se lanza error

--- src/main/test/edu/fiuba/algo3/entrega_1/DeckBuilderTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/DeckBuilderTest.java
@@ -218,6 +218,62 @@ public class DeckBuilderTest {
         DeckBuilder constructor = new DeckBuilder();
         int minimoCartas = 21;
 
+        constructor.selectCard(new Decoy("Decoy", ""));
+        constructor.selectCard(new Mardroeme("Mardroeme", ""));
+        constructor.selectCard(new Mardroeme("Mardroeme", ""));
+
+        constructor.selectCard(new Unit("Botchling", "", 4, new CloseCombat()));
+        constructor.selectCard(new Unit("Berserker", "", 4, new CloseCombat()));
+        constructor.selectCard(new Unit("Dandelion", "", 2, new CloseCombat()));
+        constructor.selectCard(new Unit("Dandelion", "", 2, new CloseCombat()));
+        constructor.selectCard(new Unit("Griffin", "", 5, new CloseCombat()));
+        constructor.selectCard(new Unit("Cerys", "", 10, new CloseCombat()));
+        constructor.selectCard(new Unit("Cow", "", 0, new Ranged()));
+        constructor.selectCard(new Unit("Dethmold", "", 6, new Ranged()));
+        constructor.selectCard(new Unit("Harpy", "", 2, new Ranged()));
+        constructor.selectCard(new Unit("Toad", "", 7, new Ranged()));
+        constructor.selectCard(new Unit("Cynthia", "", 4, new Ranged()));
+        constructor.selectCard(new Unit("Ermion", "", 8, new Ranged()));
+        constructor.selectCard(new Unit("Ballista", "", 6, new Siege()));
+        constructor.selectCard(new Unit("Catapult", "", 8, new Siege()));
+        constructor.selectCard(new Unit("Thaler", "", 1, new Siege()));
+        constructor.selectCard(new Unit("Ice Giant", "", 5, new Siege()));
+
+        assertThrows(IllegalArgumentException.class, constructor::buildDeck);
+    }
+
+    @Test
+    public void testNoSePuedeCrearElMazoSiNoHaySuficientesCartasUnidades() {
+        DeckBuilder constructor = new DeckBuilder();
+        int minimoUnidades = 15;
+
+        constructor.selectCard(new Decoy("Decoy", ""));
+        constructor.selectCard(new Mardroeme("Mardroeme", ""));
+        constructor.selectCard(new Mardroeme("Mardroeme", ""));
+        constructor.selectCard(new Scorch("Scorch", ""));
+        constructor.selectCard(new CommandersHorn("Commander's Horn", ""));
+        constructor.selectCard(new BitingFrost("Biting Frost", ""));
+        constructor.selectCard(new TorrentialRain("Torrential Rain", ""));
+        constructor.selectCard(new TorrentialRain("Torrential Rain", ""));
+
+        constructor.selectCard(new Unit("Botchling", "", 4, new CloseCombat()));
+        constructor.selectCard(new Unit("Berserker", "", 4, new CloseCombat()));
+        constructor.selectCard(new Unit("Dandelion", "", 2, new CloseCombat()));
+        constructor.selectCard(new Unit("Dandelion", "", 2, new CloseCombat()));
+        constructor.selectCard(new Unit("Griffin", "", 5, new CloseCombat()));
+
+        assertThrows(IllegalArgumentException.class, constructor::buildDeck);
+    }
+
+    @Test
+    public void testNoSePuedeCrearElMazoSiNoHaySuficientesCartasEspeciales() {
+        DeckBuilder constructor = new DeckBuilder();
+        int minimoEspeciales = 6;
+
+        constructor.selectCard(new Decoy("Decoy", ""));
+        constructor.selectCard(new Mardroeme("Mardroeme", ""));
+        constructor.selectCard(new Mardroeme("Mardroeme", ""));
+
         constructor.selectCard(new Unit("Botchling", "", 4, new CloseCombat()));
         constructor.selectCard(new Unit("Berserker", "", 4, new CloseCombat()));
         constructor.selectCard(new Unit("Dandelion", "", 2, new CloseCombat()));
