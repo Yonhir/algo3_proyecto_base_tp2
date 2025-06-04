751fc2b2e3154b546564e10dedda0b268d922484
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-04 17:03:18 -0300

    obtener cantidad cartas

--- src/main/java/edu/fiuba/algo3/Deck.java
+++ src/main/java/edu/fiuba/algo3/Deck.java
@@ -1,10 +1,24 @@
 package edu.fiuba.algo3;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Deck extends CardCollection {
-    public Deck(List<Card> cards) {
-        super(cards);
+    private List<Card> units;
+    private List<Card> specials;
+
+    public Deck(List<Card> units, List<Card> specials) {
+        super(new ArrayList<>());
+        this.units = units;
+        this.specials = specials;
+    }
+
+    public int getUnitsCount() {
+        return this.units.size();
+    }
+
+    public int getSpecialsCount() {
+        return this.specials.size();
     }
 
 }
