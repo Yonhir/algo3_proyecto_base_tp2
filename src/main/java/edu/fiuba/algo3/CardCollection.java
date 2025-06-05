31dfe14d75749ac8db54ccefbb07698dcfcc1221
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-04 22:18:39 -0300

    separacion en dos listas

--- src/main/java/edu/fiuba/algo3/CardCollection.java
+++ src/main/java/edu/fiuba/algo3/CardCollection.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class CardCollection {
-    private List<Card> cards;
+    protected List<Card> units;
+    protected List<Card> specials;
 
-    public CardCollection(List<Card> cards) {
-        this.cards = cards;
+    public CardCollection(List<Card> units, List<Card> specials) {
+        this.units = units;
+        this.specials = specials;
     }
 
     public abstract int getCardCount();
