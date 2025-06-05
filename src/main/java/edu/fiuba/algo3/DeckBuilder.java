6262be9e18c6dc433570c85cd6b08b205fad6f82
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-05 17:44:23 -0300

    creacion de las cartas en la implementacion

--- src/main/java/edu/fiuba/algo3/DeckBuilder.java
+++ src/main/java/edu/fiuba/algo3/DeckBuilder.java
@@ -20,6 +20,39 @@ public class DeckBuilder {
                 new Validate6SpecialCards(specials),
                 new Validate15UnitCards(units)
         );
+        createCards();
+    }
+
+    private void createCards() {
+        cards.add(new Decoy("Decoy", ""));
+        cards.add(new Mardroeme("Mardroeme", ""));
+        cards.add(new Mardroeme("Mardroeme", ""));
+        cards.add(new Scorch("Scorch", ""));
+        cards.add(new CommandersHorn("Commander's Horn", ""));
+        cards.add(new BitingFrost("Biting Frost", ""));
+        cards.add(new TorrentialRain("Torrential Rain", ""));
+        cards.add(new TorrentialRain("Torrential Rain", ""));
+
+        cards.add(new Unit("Botchling", "", 4, new CloseCombat()));
+        cards.add(new Unit("Berserker", "", 4, new CloseCombat()));
+        cards.add(new Unit("Dandelion", "", 2, new CloseCombat()));
+        cards.add(new Unit("Dandelion", "", 2, new CloseCombat()));
+        cards.add(new Unit("Griffin", "", 5, new CloseCombat()));
+        cards.add(new Unit("Cerys", "", 10, new CloseCombat()));
+        cards.add(new Unit("Cow", "", 0, new Ranged()));
+        cards.add(new Unit("Dethmold", "", 6, new Ranged()));
+        cards.add(new Unit("Harpy", "", 2, new Ranged()));
+        cards.add(new Unit("Toad", "", 7, new Ranged()));
+        cards.add(new Unit("Cynthia", "", 4, new Ranged()));
+        cards.add(new Unit("Ermion", "", 8, new Ranged()));
+        cards.add(new Unit("Ballista", "", 6, new Siege()));
+        cards.add(new Unit("Catapult", "", 8, new Siege()));
+        cards.add(new Unit("Thaler", "", 1, new Siege()));
+        cards.add(new Unit("Ice Giant", "", 5, new Siege()));
+    }
+
+    public List<Card> getCards() {
+        return cards;
     }
 
     public Deck buildDeck() {
@@ -39,9 +72,24 @@ public class DeckBuilder {
         specials.add(card);
     }
 
+    public void unselectUnitCard(Card card) {
+        units.remove(card);
+    }
+
+    public void unselectSpecialCard(Card card) {
+        specials.remove(card);
+    }
+
     public void selectCard(Card card) {
-        selectedCards.add(card);
+        if (this.cards.remove(card)) {
+            selectedCards.add(card);
+        } //si no está, lanzar excepcion
         card.selectInDeckBuilder(this);
+    }
+
+    public void unselectCard(Card card) {
+        selectedCards.remove(card);
+        card.unselectInDeckBuilder(this);
     }
 
     public List<Card> getSelection() {
