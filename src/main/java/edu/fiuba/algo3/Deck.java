b860425dbf9a92ffeb24645eb8c75aa9d6729807
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-04 22:19:05 -0300

    separacion en dos listas

--- src/main/java/edu/fiuba/algo3/Deck.java
+++ src/main/java/edu/fiuba/algo3/Deck.java
@@ -4,13 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Deck extends CardCollection {
-    private List<Card> units;
-    private List<Card> specials;
 
     public Deck(List<Card> units, List<Card> specials) {
-        super(new ArrayList<>());
-        this.units = units;
-        this.specials = specials;
+        super(units, specials);
     }
 
     public int getCardCount() {
