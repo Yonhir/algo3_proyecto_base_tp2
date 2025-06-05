f4a17f1cd8a25dc8aba6ad06bd9debf7ff9ab96a
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-04 22:37:43 -0300

    ctes ya no necesitadas

--- src/main/java/edu/fiuba/algo3/DeckBuilder.java
+++ src/main/java/edu/fiuba/algo3/DeckBuilder.java
@@ -5,9 +5,6 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class DeckBuilder {
-    public static final int MIN_UNITS = 15;
-    public static final int MIN_SPECIALS = 6;
-
     private List<Card> cards;
     private List<Card> selectedCards;
     private List<Card> units;
