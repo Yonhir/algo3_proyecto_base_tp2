8873ec029417769724afc98f4ec1bab56b6028ab
JuaniMoore <jmoore@fi.uba.ar>
2025-06-06 16:27:20 -0300

    Borro archivos viejos

--- src/main/java/edu/fiuba/algo3/RowType.java
+++
@@ -1,6 +0,0 @@
-package edu.fiuba.algo3;
-
-public abstract class RowType {
-    public abstract void placeCardInRow(Row row, Card card, RowType selectedRowType);
-    public abstract int calculateTotalPoints(Row row, RowType rowType);
-}
