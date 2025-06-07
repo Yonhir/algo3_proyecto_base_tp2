a36159c316c8f576230753d7639768f206dcc8f4
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-07 16:43:44 -0300

    constantes

--- src/main/java/edu/fiuba/algo3/modelo/Deck.java
+++ src/main/java/edu/fiuba/algo3/modelo/Deck.java
@@ -3,8 +3,10 @@ package edu.fiuba.algo3.modelo;
 import java.util.List;
 
 public class Deck extends CardCollection {
-    List<Card> units;
-    List<Card> specials;
+    private static final int MIN_UNITS = 15;
+    private static final int MIN_SPECIALS = 6;
+    private List<Card> units;
+    private List<Card> specials;
 
     public Deck(List<Card> cards, List<Card> units, List<Card> specials) {
         super(cards);
@@ -14,7 +16,7 @@ public class Deck extends CardCollection {
     }
 
     private void validate() {
-        if ((units.size() < 15) || (specials.size() < 6)) {
+        if ((units.size() < MIN_UNITS) || (specials.size() < MIN_SPECIALS)) {
             throw new IllegalArgumentException();
         }
     }
