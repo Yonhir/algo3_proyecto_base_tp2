d5dee87dd4b52d7896e4d22e979a8824d84d524b
Yonhir <yonhir@hotmail.com>
2025-06-09 12:49:54 -0300

    undo check

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,5 @@ module edu.fiuba.algo3 {
     exports edu.fiuba.algo3;
     exports edu.fiuba.algo3.vistas;
     exports edu.fiuba.algo3.modelo;
-    //opens edu.fiuba.algo3.modelo;
+    opens edu.fiuba.algo3.modelo;
 }
