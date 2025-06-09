3f53a61d5993927090baf6a214e489a4bfaa2b16
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-09 12:42:52 -0300

    deshago cambio

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,4 @@ module edu.fiuba.algo3 {
     requires json.simple;
     exports edu.fiuba.algo3;
     exports edu.fiuba.algo3.vistas;
-    opens edu.fiuba.algo3.modelo to org.junit.platform.commons;
 }
