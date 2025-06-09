be5e52aaff40508776ee19e76c5d0119602660d6
JuaniMoore <jmoore@fi.uba.ar>
2025-06-09 14:35:55 -0300

    Issue 8 - Verificar que un jugador pueda colocar una carta en una sección del tablero.

---
+++ src/main/java/edu/fiuba/algo3/modelo/Card.java
@@ -0,0 +1,15 @@
+package edu.fiuba.algo3.modelo;
+
+public abstract class Card {
+    protected String name;
+    protected String description;
+
+    public Card(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public abstract void play(Row row);
+
+    public abstract boolean canBePlaced(Row row);
+}
