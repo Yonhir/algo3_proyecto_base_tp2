f4650f1a657224484fd7a42badecc421f734cac3
Yonhir <yonhir@hotmail.com>
2025-06-09 16:07:27 -0300

    remove untested code

--- src/main/java/edu/fiuba/algo3/modelo/Card.java
+++ src/main/java/edu/fiuba/algo3/modelo/Card.java
@@ -12,13 +12,5 @@ public abstract class Card {
 
     public abstract void play(Row row);
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
     public abstract boolean canBePlaced(Row row);
 }
