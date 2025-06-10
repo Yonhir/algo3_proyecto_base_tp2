d4d476cc69ba7cf8404d6c71f1712be0735ad28a
Yonhir <yonhir@hotmail.com>
2025-06-10 14:56:15 -0300

    add missing CardCollection methods

--- src/main/java/edu/fiuba/algo3/modelo/CardCollection.java
+++ src/main/java/edu/fiuba/algo3/modelo/CardCollection.java
@@ -8,4 +8,12 @@ public abstract class CardCollection {
     public CardCollection(List<Card> cards) {
         this.cards = cards;
     }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int getCardCount() {
+        return cards.size();
+    }
 }
