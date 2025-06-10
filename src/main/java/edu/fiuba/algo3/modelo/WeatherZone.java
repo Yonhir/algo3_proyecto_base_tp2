14f9cd7549b08fc2bf77854ea0f35c09fa622689
Yonhir <yonhir@hotmail.com>
2025-06-10 14:46:14 -0300

    add weatherZone and their tests

---
+++ src/main/java/edu/fiuba/algo3/modelo/WeatherZone.java
@@ -0,0 +1,48 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.List;
+
+public class WeatherZone implements CardTarget {
+    private final List<Row> closeCombatRows;
+    private final List<Row> rangedRows;
+    private final List<Row> siegeRows;
+
+    public WeatherZone(List<Row> closeCombatRows, List<Row> rangedRows, List<Row> siegeRows) {
+        this.closeCombatRows = closeCombatRows;
+        this.rangedRows = rangedRows;
+        this.siegeRows = siegeRows;
+    }
+
+    @Override
+    public void placeCard(Card card) {
+        if (!card.canBePlaced(this)) {
+            throw new IllegalArgumentException("La carta no puede colocarse en esta zona de clima.");
+        }
+        card.play(this);
+    }
+
+    @Override
+    public void addCard(Card card) {
+        if (card instanceof Weather) {
+            ((Weather) card).addToWeatherZone(this);
+        }
+    }
+
+    public void addCloseCombatWeather(Weather weather) {
+        for (Row row : closeCombatRows) {
+            row.addWeather(weather);
+        }
+    }
+
+    public void addRangedWeather(Weather weather) {
+        for (Row row : rangedRows) {
+            row.addWeather(weather);
+        }
+    }
+
+    public void addSiegeWeather(Weather weather) {
+        for (Row row : siegeRows) {
+            row.addWeather(weather);
+        }
+    }
+} 
