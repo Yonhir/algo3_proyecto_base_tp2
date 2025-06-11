dcd05b08f1f1d22b1dbf884c6631c6a92cb4a3c3
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-11 16:13:07 -0300

    setUp

--- src/main/test/edu/fiuba/algo3/modelo/ScorchTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/ScorchTest.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.modelo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,22 +9,29 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ScorchTest {
-    @Test
-    public void testSeJuegaUnaTierraArrasadaSeEliminanLasCartasMasFuertesDeLaFilaCloseCombatCorrectamente() {
-        Row cuerpoACuerpo = new CloseCombat();
-        Row rango = new Ranged();
-        Row asedio = new Siege();
-        SpecialZone zonaDeEspeciales = new SpecialZone(List.of(cuerpoACuerpo), List.of(rango), List.of(asedio));
+    private Row cuerpoACuerpo;
+    private Row rango;
+    private Row asedio;
 
-        Card tierraArrasada = new Scorch("Nombre", "Descripcion");
+    private Card unidad2;
+    private Card unidad5;
+    private Card unidad7;
+    private Card unidad8;
+
+    @BeforeEach
+    void setUp() {
+        cuerpoACuerpo = new CloseCombat();
+        rango = new Ranged();
+        asedio = new Siege();
+
         Card unidad1 = new Unit("Griffin", "Descripcion", 5, true, false, false, new ArrayList<Modifier>());
-        Card unidad2 = new Unit("Fiend", "Descripcion", 6, true, false, false, new ArrayList<Modifier>());
+        unidad2 = new Unit("Fiend", "Descripcion", 6, true, false, false, new ArrayList<Modifier>());
         Card unidad3 = new Unit("Donar an Hindar", "Descripcion", 4, true, false, false, new ArrayList<Modifier>());
         Card unidad4 = new Unit("Cynthia", "Descripcion", 4, false, true, false, new ArrayList<Modifier>());
-        Card unidad5 = new Unit("Dethmold", "Descripcion", 6, false, true, false, new ArrayList<Modifier>());
+        unidad5 = new Unit("Dethmold", "Descripcion", 6, false, true, false, new ArrayList<Modifier>());
         Card unidad6 = new Unit("Endrega", "Descripcion", 2, false, true, false, new ArrayList<Modifier>());
-        Card unidad7 = new Unit("Ballista", "Descripcion", 6, false, false, true, new ArrayList<Modifier>());
-        Card unidad8 = new Unit("Earth Elemental", "Descripcion", 6, false, false, true, new ArrayList<Modifier>());
+        unidad7 = new Unit("Ballista", "Descripcion", 6, false, false, true, new ArrayList<Modifier>());
+        unidad8 = new Unit("Earth Elemental", "Descripcion", 6, false, false, true, new ArrayList<Modifier>());
         Card unidad9 = new Unit("Ice Giant", "Descripcion", 5, false, false, true, new ArrayList<Modifier>());
 
         cuerpoACuerpo.placeCard(unidad1);
@@ -35,6 +43,12 @@ public class ScorchTest {
         asedio.placeCard(unidad7);
         asedio.placeCard(unidad8);
         asedio.placeCard(unidad9);
+    }
+
+    @Test
+    public void testSeJuegaUnaTierraArrasadaSeEliminanLasCartasMasFuertesDeLaFilaCloseCombatCorrectamente() {
+        SpecialZone zonaDeEspeciales = new SpecialZone(List.of(cuerpoACuerpo), List.of(rango), List.of(asedio));
+        Card tierraArrasada = new Scorch("Nombre", "Descripcion");
 
         zonaDeEspeciales.placeCard(tierraArrasada);
 
@@ -43,31 +57,8 @@ public class ScorchTest {
 
     @Test
     public void testSeJuegaUnaTierraArrasadaSeEliminanLasCartasMasFuertesDeLaFilaRangedCorrectamente() {
-        Row cuerpoACuerpo = new CloseCombat();
-        Row rango = new Ranged();
-        Row asedio = new Siege();
         SpecialZone zonaDeEspeciales = new SpecialZone(List.of(cuerpoACuerpo), List.of(rango), List.of(asedio));
-
         Card tierraArrasada = new Scorch("Nombre", "Descripcion");
-        Card unidad1 = new Unit("Griffin", "Descripcion", 5, true, false, false, new ArrayList<Modifier>());
-        Card unidad2 = new Unit("Fiend", "Descripcion", 6, true, false, false, new ArrayList<Modifier>());
-        Card unidad3 = new Unit("Donar an Hindar", "Descripcion", 4, true, false, false, new ArrayList<Modifier>());
-        Card unidad4 = new Unit("Cynthia", "Descripcion", 4, false, true, false, new ArrayList<Modifier>());
-        Card unidad5 = new Unit("Dethmold", "Descripcion", 6, false, true, false, new ArrayList<Modifier>());
-        Card unidad6 = new Unit("Endrega", "Descripcion", 2, false, true, false, new ArrayList<Modifier>());
-        Card unidad7 = new Unit("Ballista", "Descripcion", 6, false, false, true, new ArrayList<Modifier>());
-        Card unidad8 = new Unit("Earth Elemental", "Descripcion", 6, false, false, true, new ArrayList<Modifier>());
-        Card unidad9 = new Unit("Ice Giant", "Descripcion", 5, false, false, true, new ArrayList<Modifier>());
-
-        cuerpoACuerpo.placeCard(unidad1);
-        cuerpoACuerpo.placeCard(unidad2);
-        cuerpoACuerpo.placeCard(unidad3);
-        rango.placeCard(unidad4);
-        rango.placeCard(unidad5);
-        rango.placeCard(unidad6);
-        asedio.placeCard(unidad7);
-        asedio.placeCard(unidad8);
-        asedio.placeCard(unidad9);
 
         zonaDeEspeciales.placeCard(tierraArrasada);
 
@@ -76,31 +67,8 @@ public class ScorchTest {
 
     @Test
     public void testSeJuegaUnaTierraArrasadaSeEliminanLasCartasMasFuertesDeLaFilaSiegeCorrectamente() {
-        Row cuerpoACuerpo = new CloseCombat();
-        Row rango = new Ranged();
-        Row asedio = new Siege();
         SpecialZone zonaDeEspeciales = new SpecialZone(List.of(cuerpoACuerpo), List.of(rango), List.of(asedio));
-
         Card tierraArrasada = new Scorch("Nombre", "Descripcion");
-        Card unidad1 = new Unit("Griffin", "Descripcion", 5, true, false, false, new ArrayList<Modifier>());
-        Card unidad2 = new Unit("Fiend", "Descripcion", 6, true, false, false, new ArrayList<Modifier>());
-        Card unidad3 = new Unit("Donar an Hindar", "Descripcion", 4, true, false, false, new ArrayList<Modifier>());
-        Card unidad4 = new Unit("Cynthia", "Descripcion", 4, false, true, false, new ArrayList<Modifier>());
-        Card unidad5 = new Unit("Dethmold", "Descripcion", 6, false, true, false, new ArrayList<Modifier>());
-        Card unidad6 = new Unit("Endrega", "Descripcion", 2, false, true, false, new ArrayList<Modifier>());
-        Card unidad7 = new Unit("Ballista", "Descripcion", 6, false, false, true, new ArrayList<Modifier>());
-        Card unidad8 = new Unit("Earth Elemental", "Descripcion", 6, false, false, true, new ArrayList<Modifier>());
-        Card unidad9 = new Unit("Ice Giant", "Descripcion", 5, false, false, true, new ArrayList<Modifier>());
-
-        cuerpoACuerpo.placeCard(unidad1);
-        cuerpoACuerpo.placeCard(unidad2);
-        cuerpoACuerpo.placeCard(unidad3);
-        rango.placeCard(unidad4);
-        rango.placeCard(unidad5);
-        rango.placeCard(unidad6);
-        asedio.placeCard(unidad7);
-        asedio.placeCard(unidad8);
-        asedio.placeCard(unidad9);
 
         zonaDeEspeciales.placeCard(tierraArrasada);
 
