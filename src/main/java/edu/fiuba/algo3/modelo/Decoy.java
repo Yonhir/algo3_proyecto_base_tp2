1dc329719a189162a3153d92b4ef52f0d44d79ec
Yonhir <yonhir@hotmail.com>
2025-06-12 12:01:44 -0300

    Remove unnessart classes

--- src/main/java/edu/fiuba/algo3/modelo/Decoy.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3.modelo;
-
-public class Decoy extends Special {
-    public Decoy(String name, String description) {
-        super(name, description);
-    }
-}
--- src/main/java/edu/fiuba/algo3/modelo/Player.java
+++ src/main/java/edu/fiuba/algo3/modelo/Player.java
@@ -5,21 +5,36 @@ import java.util.ArrayList;
 public class Player {
     private final String name;
     private int health;
-    private final CardCollection discardPile;
-    private final CardCollection hand;
+    private final DiscardPile discardPile;
+    private final Hand hand;
+    private final Deck deck;
+    private final SpecialZone specialZone;
+    private final CloseCombat closeCombat;
+    private final Ranged ranged;
+    private final Siege siege;
 
-    public Player(String name, int health) {
+    public Player(String name, int health, Deck deck, SpecialZone specialZone, CloseCombat closeCombat, Ranged ranged, Siege siege) {
         this.name = name;
         this.health = health;
         discardPile = new DiscardPile();
         hand = new Hand(new ArrayList<>());
+        this.deck = deck;
+        this.specialZone = specialZone;
+        this.closeCombat = closeCombat;
+        this.ranged = ranged;
+        this.siege = siege;
     }
 
     public DiscardPile getDiscardPile() {
-        return (DiscardPile) discardPile;
+        return discardPile;
     }
 
     public Hand getHand(){
-        return (Hand) hand;
+        return hand;
+    }
+
+    public int calculatePoints() {
+        int points = closeCombat.calculatePoints() + ranged.calculatePoints() + siege.calculatePoints();
+        return points;
     }
 }
