ed5d1b1f73ec937103d9ea88d67c683f06b4fb77
Gabosawn <gnarvaez@fi.uba.ar>
2025-06-18 14:52:14 -0300

    MERGE TESTING SOLVED

--- src/main/java/edu/fiuba/algo3/modelo/MoraleBoost.java
+++ src/main/java/edu/fiuba/algo3/modelo/MoraleBoost.java
@@ -1,26 +1,20 @@
 package edu.fiuba.algo3.modelo;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MoraleBoost extends Special {
-    private Row[] rows;
 
-    public MoraleBoost(String name, String description, Row... rows) {
-        super(name, description);
-        this.rows = rows;
+    public MoraleBoost(String name, String description, List<SectionType> sectionTypes) {
+        super(name, description, sectionTypes);
     }
 
     @Override
-    public void play(CardTarget target) {
-        if (target instanceof Row){
-            Row row = (Row) target;
-            if(Arrays.stream(rows).noneMatch(row1 -> row1.equals(row))) throw new CardPlayInOpponentPlaceException("Morale Boost can only be played in your own rows.");
-            List<Card> cards= row.getCards();
-            for (Card card : cards) {
-                int points = ((Unit) card).calculatePoints();
-                ((Unit) card).setPoints(points * 2);
-            }
+    public void play(Section section) {
+        Row row = (Row) section;
+        List<Card> cards= row.getCards();
+        for (Card card : cards) {
+            int points = ((Unit) card).calculatePoints();
+            ((Unit) card).setPoints(points * 2);
         }
     }
 }
