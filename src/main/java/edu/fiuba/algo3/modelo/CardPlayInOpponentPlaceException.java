5f4950a7e8466322006bb29192d7260b398731d7
Gabosawn <gnarvaez@fi.uba.ar>
2025-06-18 14:54:06 -0300

    MERGE TESTING SOLVED

--- src/main/java/edu/fiuba/algo3/modelo/CardPlayInOpponentPlaceException.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3.modelo;
-
-public class CardPlayInOpponentPlaceException extends RuntimeException {
-    public CardPlayInOpponentPlaceException(String message) {
-        super(message);
-    }
-}
