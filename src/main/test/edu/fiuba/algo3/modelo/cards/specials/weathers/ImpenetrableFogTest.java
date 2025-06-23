006724b80248c118c365892609cf405da70c4e5c
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-06-23 20:21:35 -0300

    tests para la clase Impenetrable Fog

---
+++ src/main/test/edu/fiuba/algo3/modelo/cards/specials/weathers/ImpenetrableFogTest.java
@@ -0,0 +1,62 @@
+package edu.fiuba.algo3.modelo.cards.specials.weathers;
+
+import edu.fiuba.algo3.modelo.cards.units.Unit;
+import edu.fiuba.algo3.modelo.cards.units.modifiers.MoraleBoostModifier;
+import edu.fiuba.algo3.modelo.sections.Section;
+import edu.fiuba.algo3.modelo.sections.SpecialZone;
+import edu.fiuba.algo3.modelo.sections.rows.CloseCombat;
+import edu.fiuba.algo3.modelo.sections.rows.Ranged;
+import edu.fiuba.algo3.modelo.sections.rows.Siege;
+import edu.fiuba.algo3.modelo.sections.types.CloseCombatType;
+import edu.fiuba.algo3.modelo.sections.types.RangedType;
+import edu.fiuba.algo3.modelo.sections.types.SiegeType;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ImpenetrableFogTest {
+    @Test
+    public void testLaCartaImpenetrableFogSeAplicaCorrectamenteEnLasFilasRanged() {
+        Weather fog = new ImpenetrableFog("niebla", "impenetrable");
+        Ranged ranged1 = new Ranged();
+        Ranged ranged2 = new Ranged();
+        Unit carta = new Unit("carta", "comun", 6, new RangedType(), List.of(new MoraleBoostModifier()));
+
+        ranged1.placeCard(carta);
+        ranged2.placeCard(carta);
+
+        fog.apply(carta, ranged1);
+        fog.apply(carta, ranged2);
+
+        assertEquals(1, carta.calculatePoints());
+    }
+
+    @Test
+    public void testLaCartaTorrentialRainUnicamenteAfectaALasFilasSiege() {
+        Weather fog = new ImpenetrableFog("niebla", "impenetrable");
+        CloseCombat closeCombat1 = new CloseCombat();
+        CloseCombat closeCombat2 = new CloseCombat();
+        Ranged ranged1 = new Ranged();
+        Ranged ranged2 = new Ranged();
+        Siege siege1 = new Siege();
+        Siege siege2 = new Siege();
+        Section specialZone = new SpecialZone(closeCombat1, ranged1, siege1, closeCombat2, ranged2, siege2);
+        Unit carta1 = new Unit("carta", "comun", 6, new SiegeType(), List.of(new MoraleBoostModifier()));
+        Unit carta2 = new Unit("nombre", "descripcion", 7, new CloseCombatType(), List.of(new MoraleBoostModifier()));
+        Unit carta3 = new Unit("nombre", "descripcion", 5, new RangedType(), List.of(new MoraleBoostModifier()));
+
+        closeCombat1.placeCard(carta2);
+        closeCombat2.placeCard(carta2);
+        ranged1.placeCard(carta3);
+        ranged2.placeCard(carta3);
+        siege1.placeCard(carta1);
+        siege2.placeCard(carta1);
+        fog.play(specialZone);
+
+        assertEquals(6, carta1.calculatePoints());
+        assertEquals(7, carta2.calculatePoints());
+        assertEquals(1, carta3.calculatePoints());
+    }
+}
