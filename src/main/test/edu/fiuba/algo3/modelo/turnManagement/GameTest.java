799e90accce2015e3e528161ca681c13bfb2561d
AndyAmes <aames@fi.uba.ar>
2025-06-25 23:24:34 -0300

    test cuando termina la ronda las cartas van a su correspondiente discardPile

--- src/main/test/edu/fiuba/algo3/modelo/turnManagement/GameTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/turnManagement/GameTest.java
@@ -1,5 +1,7 @@
 package edu.fiuba.algo3.modelo.turnManagement;
 
+import edu.fiuba.algo3.modelo.cards.specials.weathers.TorrentialRain;
+import edu.fiuba.algo3.modelo.cards.units.modifiers.Modifier;
 import edu.fiuba.algo3.modelo.colors.*;
 import edu.fiuba.algo3.modelo.cardcollections.Deck;
 import edu.fiuba.algo3.modelo.cardcollections.DiscardPile;
@@ -18,6 +20,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -40,8 +44,21 @@ public class GameTest {
     private DiscardPile discardPile1;
     private DiscardPile discardPile2;
 
+    private Deck deck1;
+    private Deck deck2;
+    private List<Card> especiales;
+    private List<Card> unidades;
+    private Card unit1;
+    private Card special2;
     @BeforeEach
     public void setUp() {
+        unit1 = new Unit("Nombre", "Descripcion", 4, new CloseCombatType(), new ArrayList<Modifier>());
+        special2 = new TorrentialRain("Nombre", "Descripcion");
+        deck1 = new Deck();
+        deck2 = new Deck();
+        deck1.addCard(unit1);
+        deck2.addCard(special2);
+
         discardPile1 = new DiscardPile();
         discardPile2 = new DiscardPile();
         player1CloseCombatRow = new CloseCombat(discardPile1);
@@ -58,8 +75,8 @@ public class GameTest {
 
 
         round = new Round(player1, player2);
-        player1 = new Player("nombre1", new Deck(), discardPile1, player1CloseCombatRow, player1RangedRow, player1SiegeRow, new Blue());
-        player2 = new Player("nombre2", new Deck(), discardPile2, player2CloseCombatRow, player2RangedRow, player2SiegeRow, new Red());
+        player1 = new Player("nombre1", deck1, discardPile1, player1CloseCombatRow, player1RangedRow, player1SiegeRow, new Blue());
+        player2 = new Player("nombre2", deck2, discardPile2, player2CloseCombatRow, player2RangedRow, player2SiegeRow, new Red());
         game = new Game(player1, player2, specialZone);
     }
 
@@ -111,5 +128,20 @@ public class GameTest {
         Game game = new Game(p1, p2, mock3);
 
         assertTrue(game.bothPlayersWonARound());
+    }
+
+    @Test
+    public void testClearBoardAndAllCardsGoToTheirCorrespondingDiscardPile(){
+        player1CloseCombatRow.placeCard(unit1,round);
+        specialZone.placeCard(special2, round);
+
+        game.passRound();
+        game.passRound();
+
+        assertTrue(discardPile1.containsCard(unit1));
+        assertFalse(discardPile1.containsCard(special2));
+        assertTrue(discardPile2.containsCard(special2));
+        assertFalse(discardPile2.containsCard(unit1));
+
     }
 }
