a58018be4d62caa364c64f7e301d8979e877fbe9
AndyAmes <aames@fi.uba.ar>
2025-07-13 13:17:46 -0300

    el jugador ahora solo puede descartar una carta a la vez

--- src/main/java/edu/fiuba/algo3/views/components/DiscardCardDialog.java
+++ src/main/java/edu/fiuba/algo3/views/components/DiscardCardDialog.java
@@ -1,12 +1,10 @@
 package edu.fiuba.algo3.views.components;
 
 import edu.fiuba.algo3.controllers.ButtonConfirmDiscardHandler;
-import edu.fiuba.algo3.controllers.ButtonOmitirDiscardHandler;
+import edu.fiuba.algo3.controllers.ButtonDiscardHandler;
 import edu.fiuba.algo3.models.cardcollections.Deck;
 import edu.fiuba.algo3.models.cardcollections.DiscardPile;
 import edu.fiuba.algo3.models.cardcollections.Hand;
-import edu.fiuba.algo3.models.cards.Card;
-import edu.fiuba.algo3.views.components.cardcomponent.card.UICard;
 import edu.fiuba.algo3.views.components.cardlist.UIHand;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -20,27 +18,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Screen;
 
-import java.util.HashSet;
-import java.util.Set;
 
 
 public class DiscardCardDialog {
-    private static Set<Card> enableCardSelection(UIHand tempUIHand) {
-        Set<Card> selectedCards = new HashSet<>();
-        for (UICard uiCard : tempUIHand.getCards()) {
-            uiCard.setOnMouseClicked(e -> {
-                Card card = uiCard.getModelCard();
-                if (selectedCards.contains(card)) {
-                    selectedCards.remove(card);
-                    uiCard.setStyle("");
-                } else if (selectedCards.size() < 2) {
-                    selectedCards.add(card);
-                    uiCard.setStyle("-fx-border-color: blue; -fx-border-width: 3;");
-                }
-            });
-        }
-        return selectedCards;
-    }
 
     public static void show(
             StackPane rootPane,
@@ -48,12 +28,12 @@ public class DiscardCardDialog {
             String playerName,
             DiscardPile discardPile,
             Deck deck,
-            Runnable onFinish)
-    {
+            Runnable onFinish) {
 
         Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
         double windowWidth = bounds.getWidth();
-        double windowHeight = bounds.getHeight(); Rectangle overlay = new Rectangle(windowWidth, windowHeight);
+        double windowHeight = bounds.getHeight();
+        Rectangle overlay = new Rectangle(windowWidth, windowHeight);
         overlay.setFill(Color.BLACK);
         overlay.setOpacity(0.4);
 
@@ -62,18 +42,18 @@ public class DiscardCardDialog {
         tempUIHand.setMaxHeight(150);
         tempUIHand.setStyle("-fx-background-color: #DEB887; -fx-border-color: #8B4513; -fx-border-width: 3;");
 
-        Set<Card> selectedCards = enableCardSelection(tempUIHand);
-
         Label titleLabel = new Label(playerName + ": Podés descartar hasta 2 cartas de tu mano");
         titleLabel.setStyle("-fx-font-size: 23px; -fx-text-fill: white; -fx-font-weight: bold;");
         titleLabel.setScaleY(1.5);
         titleLabel.setScaleX(1.2);
-        Button buttonConfirm = new Button("Confirmar");
-        buttonConfirm.setStyle("-fx-background-color: #228B22; -fx-text-fill: white; -fx-font-size: 20px; -fx-font-weight: bold;");
-        Button buttonSkip = new Button("Omitir");
-        buttonSkip.setStyle("-fx-background-color: #B22222; -fx-text-fill: white; -fx-font-size: 20px; -fx-font-weight: bold;");
 
-        HBox buttonBox = new HBox(20, buttonSkip, buttonConfirm);
+        Button buttonDescartar = new Button("Descartar");
+        buttonDescartar.setStyle("-fx-background-color: #FFA500; -fx-text-fill: black; -fx-font-size: 20px; -fx-font-weight: bold;");
+
+        Button buttonConfirmar = new Button("Confirmar");
+        buttonConfirmar.setStyle("-fx-background-color: #228B22; -fx-text-fill: white; -fx-font-size: 20px; -fx-font-weight: bold;");
+
+        HBox buttonBox = new HBox(20, buttonDescartar, buttonConfirmar);
         buttonBox.setAlignment(Pos.CENTER);
 
         VBox dialogContent = new VBox(20, titleLabel, tempUIHand, buttonBox);
@@ -84,12 +64,18 @@ public class DiscardCardDialog {
         dialogContent.setMaxHeight(600);
         dialogContent.setStyle("-fx-background-color: #C19A6B; -fx-border-color: #8B4513; -fx-border-width: 3;");
 
-        buttonConfirm.setOnAction(new ButtonConfirmDiscardHandler(rootPane, dialogContent, overlay, hand, discardPile, deck, selectedCards, onFinish));
-        buttonSkip.setOnAction(new ButtonOmitirDiscardHandler(rootPane, overlay, dialogContent, onFinish));
-
         rootPane.getChildren().addAll(overlay, dialogContent);
+
+        ButtonDiscardHandler buttonDiscardHandler = new ButtonDiscardHandler(
+                rootPane, dialogContent, hand, discardPile,
+                deck, tempUIHand, buttonDescartar);
+        buttonDescartar.setOnAction(buttonDiscardHandler);
+
+        ButtonConfirmDiscardHandler buttonConfirmDiscardHandler = new ButtonConfirmDiscardHandler(rootPane, dialogContent, overlay, onFinish);
+        buttonConfirmar.setOnAction(buttonConfirmDiscardHandler);
     }
 }
+
 
 
 
