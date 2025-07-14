6197bf89c5d2c64d66cae37bea7ee6fb34c09dea
Yonhir <yonhir@hotmail.com>
2025-07-14 18:08:08 -0300

    Update Appcontroller

--- src/main/java/edu/fiuba/algo3/views/GameView.java
+++ src/main/java/edu/fiuba/algo3/views/GameView.java
@@ -9,11 +9,10 @@ import edu.fiuba.algo3.views.components.cardlist.UIRow;
 import edu.fiuba.algo3.views.components.cardlist.UISpecialZone;
 import edu.fiuba.algo3.views.components.PassTurnButton;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Screen;
 
 public class GameView extends StackPane {
     // Layout constants
@@ -21,14 +20,9 @@ public class GameView extends StackPane {
     private static final double CENTER_COLUMN_WIDTH_RATIO = 0.50;
     private static final double RIGHT_COLUMN_WIDTH_RATIO = 0.3;
 
-    private final UIDeck playerUIDeck, opponentUIDeck;
-    private final UIDiscardPile playerUIDiscardPile, opponentUIDiscardPile;
     private final LeftColumn leftColumn;
     private final CenterColumn centerColumn;
     private final RightColumn rightColumn;
-    private final PassTurnButton passButton;
-    
-    private Scene scene;
 
     public GameView(Board board) {
         setStyle("-fx-background-color: #8B4513;");
@@ -42,26 +36,23 @@ public class GameView extends StackPane {
         UIRow playerSiege = new UIRow(board.getCurrentPlayerSiege());
 
         UISpecialZone UISpecialZoneList = new UISpecialZone(board.getSpecialZone());
-        playerUIDeck = new UIDeck(board.getCurrentPlayerDeck());
-        opponentUIDeck = new UIDeck(board.getOpponentDeck());
-        playerUIDiscardPile = new UIDiscardPile(board.getCurrentPlayerDiscardPile());
-        opponentUIDiscardPile = new UIDiscardPile(board.getOpponentDiscardPile());
-        passButton = new PassTurnButton(board.getGame());
+        UIDeck playerUIDeck = new UIDeck(board.getCurrentPlayerDeck());
+        UIDeck opponentUIDeck = new UIDeck(board.getOpponentDeck());
+        UIDiscardPile playerUIDiscardPile = new UIDiscardPile(board.getCurrentPlayerDiscardPile());
+        UIDiscardPile opponentUIDiscardPile = new UIDiscardPile(board.getOpponentDiscardPile());
+        PassTurnButton passButton = new PassTurnButton(board.getGame());
         leftColumn = new LeftColumn(UISpecialZoneList);
         centerColumn = new CenterColumn(opponentCloseCombat, opponentRanged, opponentSiege,
                 playerCloseCombat, playerRanged, playerSiege, UIHandList);
         rightColumn = new RightColumn(playerUIDeck, opponentUIDeck, playerUIDiscardPile, opponentUIDiscardPile, passButton);
+
+        // Initialize the layout
+        initializeLayout();
     }
 
-    public Scene createScene() {
-        // Get screen dimensions automatically
-        javafx.stage.Screen screen = javafx.stage.Screen.getPrimary();
-        javafx.geometry.Rectangle2D bounds = screen.getVisualBounds();
-        double windowWidth = bounds.getWidth();
-        double windowHeight = bounds.getHeight();
-
+    private void initializeLayout() {
         HBox gameBoardLayout = new HBox();
-        gameBoardLayout.setAlignment(javafx.geometry.Pos.CENTER);
+        gameBoardLayout.setAlignment(Pos.CENTER);
         gameBoardLayout.setStyle("-fx-background-color: #8B4513; -fx-border-color: #654321; -fx-border-width: 4px;");
 
         leftColumn.prefWidthProperty().bind(gameBoardLayout.widthProperty().multiply(LEFT_COLUMN_WIDTH_RATIO));
@@ -69,17 +60,13 @@ public class GameView extends StackPane {
         rightColumn.prefWidthProperty().bind(gameBoardLayout.widthProperty().multiply(RIGHT_COLUMN_WIDTH_RATIO));
         gameBoardLayout.getChildren().addAll(leftColumn, centerColumn, rightColumn);
 
-        VBox.setVgrow(leftColumn, javafx.scene.layout.Priority.ALWAYS);
-        VBox.setVgrow(rightColumn, javafx.scene.layout.Priority.ALWAYS);
-        VBox.setVgrow(gameBoardLayout, javafx.scene.layout.Priority.ALWAYS);
+        VBox.setVgrow(leftColumn, Priority.ALWAYS);
+        VBox.setVgrow(rightColumn, Priority.ALWAYS);
+        VBox.setVgrow(gameBoardLayout, Priority.ALWAYS);
 
         gameBoardLayout.prefHeightProperty().bind(heightProperty());
 
         getChildren().add(gameBoardLayout);
-
-        scene = new Scene(this, windowWidth, windowHeight);
-
-        return scene;
     }
 
     public void showExitConfirmation() {
