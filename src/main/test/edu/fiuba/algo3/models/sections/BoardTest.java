3e09e436e68a8bbeeac48e93e4113cb66e3130f7
Gabosawn <gnarvaez@fi.uba.ar>
2025-07-13 22:26:38 -0300

    Paso de tests

--- src/main/test/edu/fiuba/algo3/models/sections/BoardTest.java
+++ src/main/test/edu/fiuba/algo3/models/sections/BoardTest.java
@@ -19,6 +19,8 @@ public class BoardTest {
     public void testBoardInicializaTodosLosComponentesCorrectamente() {
         Board board = new Board("Jugador 1", "Jugador 2");
 
+        Assertions.assertNotNull(board.getCurrentPlayer());
+
         Assertions.assertNotNull(board.getCurrentPlayerHand());
         Assertions.assertNotNull(board.getOpponentHand());
 
